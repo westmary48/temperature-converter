2415b75e495390e7ef858fc7ec3614700ae63ca3
Mary West <westmarye1219@gmail.com>
2019-03-16 16:10:59 -0500

    added printToDOm function

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('CAN YOU SEE ME?')
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint
+}
+
+
+const toCelsius =  () => {
+
+}
+
+const toFahrenheit =  () => {
+
+}
+
+// Get a reference to the button element in the DOM
+const button = document.getElementById("converter");
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+const determineConverter = (e) => {
+  console.log("event", e);
+}
+
+// Assign a function to be executed when the button is clicked
+button.addEventListener("click", determineConverter);
